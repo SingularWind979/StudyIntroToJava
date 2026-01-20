e2d8ed5c06e745641069944d06a59817ddf2137c
singularwind <10204036+singularwind@user.noreply.gitee.com>
2026-01-20 19:40:12 +0800

    feat: complete lec02

---
+++ lectures/lec02/examples/Square2.java
@@ -0,0 +1,21 @@
+public class Square2 {
+    public static void main(String[] args) {
+        printSquare("hello");
+        printSquare(5.5);
+        printSquare(1);
+    }
+
+    private static void printSquare(String x) {
+        for (int i = 0; i < x.length(); i++) {
+            System.out.println(x);
+        }
+    }
+
+    private static void printSquare(double x) {
+        System.out.println(x * x);
+    }
+
+    private static void printSquare(int x) {
+        System.out.println(x * x);
+    }
+}
