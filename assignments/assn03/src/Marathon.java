18b5660dc8105686ff1960bfab1a179f6bf4918e
SingularWind <66107716+SingularWind979@users.noreply.github.com>
2026-01-21 15:13:02 +0800

    chore: adjust indentation

--- assignments/assn03/src/Marathon.java
+++ assignments/assn03/src/Marathon.java
@@ -1,16 +1,16 @@
 public class Marathon {
     public static void main(String[] arguments) {
         String[] names = {
-                "Elena", "Thomas", "Hamilton", "Suzie",
-                "Phil", "Matt", "Alex", "Emma",
-                "John", "James", "Jane", "Emily",
-                "Daniel", "Neda", "Aaron", "Kate"
+            "Elena", "Thomas", "Hamilton", "Suzie",
+            "Phil", "Matt", "Alex", "Emma",
+            "John", "James", "Jane", "Emily",
+            "Daniel", "Neda", "Aaron", "Kate"
         };
         int[] times = {
-                341, 273, 278, 329,
-                445, 402, 388, 275,
-                243, 334, 412, 393,
-                299, 343, 317, 265
+            341, 273, 278, 329,
+            445, 402, 388, 275,
+            243, 334, 412, 393,
+            299, 343, 317, 265
         };
 
         int firstRunnerIdx = getMinIdx(times);
