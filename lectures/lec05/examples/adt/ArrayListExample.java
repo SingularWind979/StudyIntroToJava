42f2b7692fd53944256fa1eb40e808ec17d02480
SingularWind <66107716+SingularWind979@users.noreply.github.com>
2026-01-22 19:38:51 +0800

    chore: format update with line breaks

--- lectures/lec05/examples/adt/ArrayListExample.java
+++ lectures/lec05/examples/adt/ArrayListExample.java
@@ -6,15 +6,18 @@ import java.util.List;
 public class ArrayListExample {
     public static void main(String[] args) {
         List<String> nameList = new ArrayList<>();
+        
         nameList.add("Evan");
         nameList.add("Eugene");
         nameList.add("Adam");
 
         System.out.println("nameList.size() = " + nameList.size());
+        
         System.out.println("nameList.get(0) = " + nameList.get(0));
         System.out.println("nameList.get(1) = " + nameList.get(1));
 
         nameList.set(0, "default");
+        
         nameList.remove(1);
 
         for (String name : nameList) {
