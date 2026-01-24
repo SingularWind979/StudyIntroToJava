f131b35ac1f70be79725329e2122c7a1ed04866d
SingularWind <66107716+SingularWind979@users.noreply.github.com>
2026-01-24 18:51:06 +0800

    chore: update javadoc
    add javadoc to constructors of Flower.java

--- lectures/lec06/examples/draw/shapes/Flower.java
+++ lectures/lec06/examples/draw/shapes/Flower.java
@@ -6,12 +6,27 @@ import java.awt.*;
 
 /**
  * Class represents a flower shape that can be drawn on a surface.
+ *
+ * @author SingularWind
  */
 public class Flower extends Shape {
+    /**
+     * Constructs a {@code Flower} with default position, velocity, size, and color.
+     * Calls the superclass constructor with default values for position, velocity, size, and color.
+     */
     public Flower() {
         super();
     }
 
+    /**
+     * Constructs a {@code Flower} with specified position, velocity, size, and color.
+     * Calls the superclass constructor with the provided parameters.
+     *
+     * @param pos  the position of the flower
+     * @param vel  the velocity of the flower
+     * @param size the size of the flower
+     * @param clr  the color of the flower
+     */
     public Flower(Vector2D pos, Vector2D vel, Vector2D size, Color clr) {
         super(pos, vel, size, clr);
     }
@@ -19,7 +34,7 @@ public class Flower extends Shape {
     /**
      * Draw the shape on the surface.
      *
-     * @param surface The surface where tbe shape is drawn on.
+     * @param surface the surface where tbe shape is drawn on
      */
     @Override
     public void draw(Graphics surface) {
